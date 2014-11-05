4c900b1a695b55ef47192db84d122c854142800a
johan <johan.calu@gmail.com>
2014-11-05 15:10:10 +0000

    de e-mail en wachtwoord vergeten afgewerkt

--- app/views/users/forgot.blade.php
+++ app/views/users/forgot.blade.php
@@ -9,17 +9,22 @@
 {{-- Content --}}
 @section('content')
 <div class="row">
-    <div class="col-md-4 col-md-offset-4">
-        {{ Form::open(array('action' => 'UserController@forgot', 'method' => 'post')) }}
+    <div class="col-xs-3"></div>
+    <div class="col-xs-6">
+        {{ Form::open(array('action' => 'UserController@forgot', 'method' => 'post', 'class'=>'form-horizontal')) }}
             
-            <h2>{{trans('users.forgotupword')}}</h2>
+            <h2 class='text-center'>{{trans('users.forgotupword')}}</h2>
             
             <div class="form-group {{ ($errors->has('email')) ? 'has-error' : '' }}">
+            	<div class="col-xs-6 text-center">
                 {{ Form::text('email', null, array('class' => 'form-control', 'placeholder' => trans('users.email'), 'autofocus')) }}
                 {{ ($errors->has('email') ? $errors->first('email') : '') }}
+                </div>
             </div>
-
-            {{ Form::submit(trans('users.resendpword'), array('class' => 'btn btn-primary'))}}
+            <div >
+            	{{ Form::submit(trans('users.resendpword'), array('class' => 'btn btn-primary'))}}
+            </div>
+            
 
   		{{ Form::close() }}
   	</div>
